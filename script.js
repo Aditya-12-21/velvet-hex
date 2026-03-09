533f47b0f407cac33e590a5c88eaa9352c5755ca
Aditya-12-21 <kumaradityaadi000@gmail.com>
2026-03-09 12:56:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -27,3 +27,36 @@ this.classList.toggle("zoom");
 });
 
 });
+let images = document.querySelectorAll(".card img");
+
+images.forEach(img => {
+
+img.addEventListener("click", function(){
+
+let popup = document.createElement("div");
+popup.style.position = "fixed";
+popup.style.top = "0";
+popup.style.left = "0";
+popup.style.width = "100%";
+popup.style.height = "100%";
+popup.style.background = "rgba(0,0,0,0.9)";
+popup.style.display = "flex";
+popup.style.justifyContent = "center";
+popup.style.alignItems = "center";
+
+let image = document.createElement("img");
+image.src = this.src;
+image.style.width = "400px";
+image.style.borderRadius = "10px";
+
+popup.appendChild(image);
+
+popup.onclick = function(){
+popup.remove();
+};
+
+document.body.appendChild(popup);
+
+});
+
+});
